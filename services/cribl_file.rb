1776484fca977c1d8b953aacd0ddb3f368eb9aa1
Sarath Mantrala <sarath23@gmail.com>
2022-09-22 10:45:13 -0700

    implement file tail in a service

---
+++ services/cribl_file.rb
@@ -0,0 +1,68 @@
+module Services
+  class CriblFile
+    MAX_LINES = 10
+    attr_accessor :filename, :location
+    attr_reader :default_lines
+
+    def initialize(location, name, default_lines = MAX_LINES)
+      @location = location
+      @filename = name
+      @default_lines = default_lines
+    end
+
+    def exists?
+      @valid ||= begin
+        return false if invalid?
+        # should we allow accessing any directories?
+        File.file?(file_path)
+      end
+    end
+
+    def process
+      return unless exists?
+
+      data = read
+      data.split("\n").reverse
+    end
+
+
+    private
+
+    # simple implementation of tail
+    def read
+      pos = 0
+      line = 0
+
+      loop do
+        pos -= 1
+        fd.seek(pos, IO::SEEK_END)
+        char = fd.read(1)
+
+        if line_break?(char)
+          line += 1
+        end
+
+        break if line > default_lines || fd.tell == 0
+      end
+
+      fd.read
+    end
+
+    def fd
+      @fd ||= File.open(file_path)
+    end
+
+    def file_path
+      File.join(location, filename)
+    end
+
+    # could filename be encoded when stored?
+    def invalid?
+      true if filename.nil? || filename.strip == ""
+    end
+
+    def line_break?(char)
+      char == "\n"
+    end
+  end
+end
---
+++ tests/cribl_file_test.rb
@@ -0,0 +1,39 @@
+ENV['APP_ENV'] = 'test'
+
+require 'test/unit'
+require 'rack/test'
+require_relative '../services/cribl_file.rb'
+require_relative '../server.rb'
+
+class CriblFileTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_exists?
+    conditions = [
+      ['sample-1.log', true, 'file exists'],
+      ['does-not-exist.log', false, 'file does not exist'],
+      [nil, false, 'nil filename'],
+      ['   ', false, 'empty filename']
+    ]
+
+    conditions.each do |c|
+      f = Services::CriblFile.new(LOG_LOCATION, c[0])
+      assert_equal(c[1], f.exists?, c[2])
+    end
+  end
+
+  def test_read
+    f = Services::CriblFile.new(LOG_LOCATION, 'sample-1.log', 2)
+    original = [
+      '03/22 08:51:06 INFO   :....mailbox_register: mailbox allocated for rsvp-udp',
+      '03/22 08:51:06 TRACE  :..entity_initialize: interface 9.67.117.98, entity for rsvp allocated and'
+    ]
+
+    assert_equal original.reverse, f.process
+  end
+end
+
